54966d1e61590af1e59f35f7b57459fb0dd0fde5
kgureye <48024387+kgureye@users.noreply.github.com>
2022-11-10 19:46:17 +0100

    Home, Login & Register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import '../src/index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
+// NOTE: Here we have removed the nested routing as the path is the same
+
 function App() {
   return (
-    <div>
-        My App
-    </div>
-  );
+    <>
+      <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+      </Router>
+      <ToastContainer />
+    </>
+  )
 }
 
-export default App;
+export default App
